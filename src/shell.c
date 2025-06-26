4bbe5b63b70ca1b79ac4263c798c50fb938f5a90
P-seudonymous <aryan.sarafhp@gmail.com>
2025-06-26 15:08:49 +0530

    working in/out

--- src/shell.c
+++ src/shell.c
@@ -1,10 +1,44 @@
-// make shell
+// gcc shell.c ../lib/linenoise.h -o shell
 #include <stdio.h>
+#include <string.h>
+#include "../lib/linenoise.h"
+#include <stdlib.h>
+
+#define PROMPT "$ "
+#define HISTORY_LEN 1024
 
 int main(){
-    printf("                          _                _          _  _ \n");
-    printf("  _ __  ___ ___  _  _  __| | ___  ___  ___| |_   ___ | || |\n");
-    printf(" | '_ \\(_-</ -_)| || |/ _` |/ _ \\|___|(_-<| ' \\ / -_)| || |\n");
-    printf(" | .__//__/\\___| \\_,_|\\__,_|\\___/     /__/|_||_|\\___||_||_|\n");
-    printf(" |_|                                                        \n");
+  
+  printf("                          _                _          _  _ \n");
+  printf("  _ __  ___ ___  _  _  __| | ___  ___  ___| |_   ___ | || |\n");
+  printf(" | '_ \\(_-</ -_)| || |/ _` |/ _ \\|___|(_-<| ' \\ / -_)| || |\n");
+  printf(" | .__//__/\\___| \\_,_|\\__,_|\\___/     /__/|_||_|\\___||_||_|\n");
+  printf(" |_|                                                        \n");
+  printf("\n");
+    
+  
+  if(!linenoiseHistorySetMaxLen(HISTORY_LEN)){
+    fprintf(stderr, "could not set linenoise command history.\n");
+    exit(1);
+  }
+
+  char *line;
+  while((line=linenoise(PROMPT)) != NULL){
+    if(strcmp(line, "exit") == 0){
+      exit(1);
+    }else{
+    fprintf(stdout, "%s\n", line);
+    linenoiseHistoryAdd(line);
+    linenoiseFree(line);
+    }
+  }
+
+
+
+
+
+
+
+
 }
+
